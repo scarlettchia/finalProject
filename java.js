de698dea80bc7b27951eb1a3501a23a3b76c1326
Scarlett Chen <scarlettchen1027@gmail.com>
2020-12-08 03:16:00 +0800

    add winPage and losePage

--- java.js
+++ java.js
@@ -77,17 +77,18 @@ function startQuiz() {
 
 /*click Ans*/
  
-
 var correct = 0;
 var clicked = false;
-const correctNum = document.getElementById('correctNum');
+const correctNumWin = document.getElementById('correctNumWin');
+const correctNumLose = document.getElementById('correctNumLose');
 const nexts = document.querySelectorAll('.next');
 const result = document.getElementById('result');
 const ansImgCorrect = document.querySelectorAll('.ansImgCorrect');
 const ansImgWrong = document.querySelectorAll('.ansImgWrong');
 
 
-correctNum.innerHTML=correct;
+correctNumWin.innerHTML=correct;
+correctNumLose.innerHTML=correct;
 for(let i = 0; i < nexts.length; i++) {
   nexts[i].hidden=true;
 }
@@ -100,11 +101,12 @@ function correctAns(){
     correct++;
     clicked = true;
   }
-  correctNum.innerHTML=correct;
-  
+  correctNumWin.innerHTML=correct;
+  correctNumLose.innerHTML=correct;
+
   for(let i = 0; i < nexts.length; i++) {
     nexts[i].hidden=false;
-    result.style.width="20vw";
+    result.style.width="25vw";
   }
 
 };
@@ -119,7 +121,7 @@ function wrongAns(){
    
   for(let i = 0; i < nexts.length; i++) {
     nexts[i].hidden=false;
-    result.style.width="20vw";
+    result.style.width="25vw";
   }
 
 };
@@ -185,9 +187,6 @@ function goToQ5(){
 
  /*result*/
  
- 
-
-
  result.addEventListener('mouseenter', () => {
    result.style.marginTop="41.5vw";
    });
@@ -195,10 +194,51 @@ function goToQ5(){
    result.style.marginTop="41vw";
    });  
  
+  const winPage=document.getElementById('section-7');
+
  function goToEnd() {
-     scrollSection(navLinks[6].dataset.link);
+  clicked=false;
+  for(let i = 0; i < ansImgCorrect.length; i++) {
+    ansImgCorrect[i].src="images/quizImages/ans.png";
+  }  
+  for(let i = 0; i < ansImgWrong.length; i++) {
+    ansImgWrong[i].src="images/quizImages/ans.png";
+  } 
+  for(let i = 0; i < nexts.length; i++) {
+    nexts[i].hidden=true;
   }
- 
+  result.style.width="0vw";
+   if(correct>=4){
+    winPage.style.display="block"; 
+    scrollSection(navLinks[6].dataset.link);
+    
+   }else{
+     
+     scrollSection(navLinks[7].dataset.link);
+    }
+  }
+
+  /*home*/
+const homeWin = document.getElementById('homeWin');
+const homeLose = document.getElementById('homeLose');
+
+homeWin.addEventListener('mouseenter', () => {
+  homeWin.style.marginTop="0.5vw";
+  });
+homeWin.addEventListener('mouseleave', () => {
+    homeWin.style.marginTop="0vw";
+  });
+homeLose.addEventListener('mouseenter', () => {
+  homeLose.style.marginTop="0.5vw";
+  });
+homeLose.addEventListener('mouseleave', () => {
+    homeLose.style.marginTop="0vw";
+  });
+
   function goToHome() {
+    correct=0;
+    winPage.style.display="none"; 
    scrollSection(navLinks[0].dataset.link);
+   
+
  }
